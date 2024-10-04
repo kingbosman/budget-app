a903afb2ef75c0c1cbd5d9e27ee23530d8a1e7ac
kingbosman <r.bosman.zakelijk@gmail.com>
2024-10-04 18:50:48 +0200

    auth pages to x-forms

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,43 +1,14 @@
 <x-layout>
     <x-slot:title>Login</x-slot:title>
     <main>
-        <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-            <form method="post" action="/login">
-                @csrf
-
-                <div class="space-y-12">
-                    <div class="border-b border-gray-900/10 pb-12">
-                        <h2 class="text-base font-semibold leading-7 text-gray-900">Login</h2>
-
-                        @error('email')
-                            <p class="text-sm text-red-500 mt-1">{{ $message }}</p>
-                        @enderror
-
-                        <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
-                            <div class="sm:col-span-4">
-                                <label for="email" class="block text-sm font-medium leading-6 text-gray-900">Email</label>
-                                <div class="mt-2">
-                                    <div class="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
-                                        <input type="text" name="email" id="email" value ="{{ old('email') }}" class="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6" placeholder="janesmith@gmail.com">
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="sm:col-span-4">
-                                <label for="password" class="block text-sm font-medium leading-6 text-gray-900">Password</label>
-                                <div class="mt-2">
-                                    <div class="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
-                                        <input type="password" name="password" id="password" class="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6">
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="mt-6 flex items-center justify-end gap-x-6">
-                    <button type="submit" class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Login</button>
-                </div>
-            </form>
+        <div class="mx-auto max-w-3xl px-4 py-6 sm:px-6 lg:px-8">
+            <h1 class="mb-5 text-3xl font-bold underline">
+                Login
+            </h1>
+            <x-forms.form method="post" button="Login">
+                <x-forms.input name="email" placeholder="john@email.com" value="{{ old('email') }}" />
+                <x-forms.input name="password" input="password" />
+            </x-forms.form>
         </div>
     </main>
 
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,74 +1,16 @@
 <x-layout>
     <x-slot:title>Login</x-slot:title>
     <main>
-        <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-            <form method="post" action="/register">
-                @csrf
-
-                <div class="space-y-12">
-                    <div class="border-b border-gray-900/10 pb-12">
-                        <h2 class="text-base font-semibold leading-7 text-gray-900">Register</h2>
-
-                        <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-8">
-
-                            <div class="sm:col-span-4">
-                                <label for="name" class="block text-sm font-medium leading-6 text-gray-900">Name</label>
-                                <div class="mt-2">
-                                    <div class="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
-                                        <input type="text" value="{{ old('name') }}" name="name" id="name" placeholder="John" class="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6">
-                                    </div>
-                                    @error('name')
-                                        <p class="text-sm text-red-500 mt-1">{{ $message }}</p>
-                                    @enderror
-                                </div>
-                            </div>
-
-                            <div class="sm:col-span-4">
-                                <label for="password" class="block text-sm font-medium leading-6 text-gray-900">Email</label>
-                                <div class="mt-2">
-                                    <div class="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
-                                        <input type="text" name="email" id="email" value="{{ old('email') }}" placeholder="John@gmail.com" class="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6">
-                                    </div>
-                                    @error('email')
-                                    <p class="text-sm text-red-500 mt-1">{{ $message }}</p>
-                                    @enderror
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-8">
-
-                            <div class="sm:col-span-4">
-                                <label for="password" class="block text-sm font-medium leading-6 text-gray-900">Password</label>
-                                <div class="mt-2">
-                                    <div class="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
-                                        <input type="password" name="password" id="password" class="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6">
-                                    </div>
-                                    @error('password')
-                                    <p class="text-sm text-red-500 mt-1">{{ $message }}</p>
-                                    @enderror
-                                </div>
-                            </div>
-
-                            <div class="sm:col-span-4">
-                                <label for="password_confirmation" class="block text-sm font-medium leading-6 text-gray-900">Password confirmation</label>
-                                <div class="mt-2">
-                                    <div class="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
-                                        <input type="password" name="password_confirmation" id="password_confirmation" class="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6">
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-
-                    </div>
-                </div>
-
-                <div class="mt-6 flex items-center justify-end gap-x-6">
-                    <button type="submit" class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Register</button>
-                </div>
-            </form>
+        <div class="mx-auto max-w-3xl px-4 py-6 sm:px-6 lg:px-8">
+            <h1 class="mb-5 text-3xl font-bold underline">
+                Register
+            </h1>
+            <x-forms.form method="post" button="Register">
+                <x-forms.input name="name" placeholder="John" value="{{ old('name') }}" />
+                <x-forms.input name="email" placeholder="John@email.com" value="{{ old('name') }}" />
+                <x-forms.input name="password" input="password" />
+                <x-forms.input name="password_confirmation" input="password" />
+            </x-forms.form>
         </div>
     </main>
-
-
 </x-layout>
